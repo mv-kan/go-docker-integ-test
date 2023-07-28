beee3e9d806ca8933c0e4e738d930552e7ed675f
Mykhailo Kan <mykhailo.kan@giss.pl>
2023-07-28 11:57:39 +0200

    network mode host

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
-# rsyslog redhat ubi9 with daemon 
-# YOU HAVE TO RUN IT IN DOCKER CONTAINER PRIVILIDGE MODE
-FROM registry.access.redhat.com/ubi9-init
+FROM golang:1.21rc3-alpine3.17
 
-RUN dnf install rsyslog -y
-RUN dnf install net-tools -y
-RUN printf "\nmodule(load=\"imtcp\")\ninput(type=\"imtcp\" port=\"10123\")\n" >> /etc/rsyslog.conf
-RUN printf "\n*.* /var/log/mytest.all\n" >> /etc/rsyslog.conf
-RUN printf "\n*.info /var/log/mytest.info\n" >> /etc/rsyslog.conf
-RUN printf "\n*.err /var/log/mytest.err\n" >> /etc/rsyslog.conf
-RUN printf "\n*.crit /var/log/mytest.crit\n" >> /etc/rsyslog.conf
+WORKDIR /app
+COPY . ./
+
+RUN go build -o sendthing-app .
+
+# EXPOSE 10123
+CMD [ "./sendthing-app" ]
--- send_thing_test.go
+++ send_thing_test.go
@@ -8,34 +8,50 @@ import (
 	"testing"
 
 	"github.com/ory/dockertest"
+	"github.com/ory/dockertest/docker"
 	"github.com/stretchr/testify/assert"
 )
 
 var (
-	LOG_HOST = "localhost"
-	LOG_PORT string
+	RSYSLOG_DAEMON_PORT = "10123"
+)
+var (
+	LOG_HOST                             = "localhost"
+	LOG_PORT                      string = RSYSLOG_DAEMON_PORT
+	RSYSLOG_DAEMON_CONTAINER_NAME        = "test-remote-logger"
+	THIS_APP_CONTAINER_NAME              = "test-sendthing"
 )
 
-func TestMain(m *testing.M) {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
-	}
-
+func runThisApp(pool *dockertest.Pool) *dockertest.Resource {
 	resource, err := pool.BuildAndRunWithOptions(
-		"./Dockerfile",
+		"./Dockerfile.debug",
 		&dockertest.RunOptions{
-			Privileged:   true,
-			Name:         "test-remote-logger",
-			ExposedPorts: []string{"10123"}})
-
+			Privileged: true,
+			Name:       THIS_APP_CONTAINER_NAME,
+		}, func(hc *docker.HostConfig) {
+			hc.NetworkMode = "host"
+		},
+	)
 	if err != nil {
 		log.Fatalf("Could not start resource: %s", err)
 	}
-	tmp := resource.GetPort("10123/tcp")
-	fmt.Println(tmp)
+	return resource
+}
+
+func runRemoteSyslogDaemon(pool *dockertest.Pool) *dockertest.Resource {
+	resource, err := pool.BuildAndRunWithOptions(
+		"./Dockerfile.rsyslog_daemon",
+		&dockertest.RunOptions{
+			Privileged: true,
+			Name:       RSYSLOG_DAEMON_CONTAINER_NAME,
+		}, func(hc *docker.HostConfig) {
+			hc.NetworkMode = "host"
+		},
+	)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
 	err = pool.Retry(func() error {
-		LOG_PORT = resource.GetPort("10123/tcp")
 		// ping to ensure that the server is up and running
 		_, err := net.Dial("tcp", net.JoinHostPort("localhost", LOG_PORT))
 		return err
@@ -43,15 +59,29 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		log.Fatalf("Could not start resource: %s", err)
 	}
+	return resource
+}
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+	allContainers := []*dockertest.Resource{}
+
+	rsyslogDaemon := runRemoteSyslogDaemon(pool)
+	allContainers = append(allContainers, rsyslogDaemon)
+
 	code := m.Run()
 
-	if err := pool.Purge(resource); err != nil {
-		log.Fatalf("Could not purge resource: %s", err)
+	// remove all containers
+	for _, container := range allContainers {
+		if err := pool.Purge(container); err != nil {
+			log.Fatalf("Could not purge resource: %s", err)
+		}
+		// hard kill the container in 3 minutes (180 Seconds)
+		container.Expire(180)
 	}
-
-	// hard kill the container in 3 minutes (180 Seconds)
-	resource.Expire(180)
-
 	os.Exit(code)
 }
 
@@ -59,4 +89,5 @@ func TestSendThingGeneral(t *testing.T) {
 	fmt.Println("Send logs to ", LOG_HOST+":"+LOG_PORT)
 	err := SendThing(LOG_HOST + ":" + LOG_PORT)
 	assert.Nil(t, err)
+
 }
